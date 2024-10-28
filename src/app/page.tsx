784bdbe8a06883ba465883bb154eba9990bdee6d
Huma-Mohsin <160951214+Huma-Mohsin@users.noreply.github.com>
2024-10-28 11:08:40 +0500

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home(){
+  return(
+    <div className="max-w-4xl  mx-auto p-5">
+      <h1 classname="text-center text-[40px]" TO Do App></h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
